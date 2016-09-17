d922e212c939d7689cc81d06b1272679d6a9db09
Simeon Kakpovi <skakpovi@gmail.com>
2016-09-17 17:21:49 -0400

    Create start.py
    Drone lifts off the ground and lands

---
+++ start.py
@@ -0,0 +1,73 @@
+print "Connecting to drone"
+# Import DroneKit-Python
+from dronekit import connect, VehicleMode
+import sys
+import time
+
+# Connect to UDP endpoint (and wait for default attributes to accumulate)
+target = sys.argv[1] if len(sys.argv) >= 2 else 'udpin:0.0.0.0:14550'
+print 'Connecting to ' + target + '...'
+vehicle = connect(target, wait_ready=True)
+
+
+# Get some vehicle attributes (state)
+print "Get some vehicle attribute values:"
+print " GPS: %s" % vehicle.gps_0
+print " Battery: %s" % vehicle.battery
+print " Last Heartbeat: %s" % vehicle.last_heartbeat
+print " Is Armable?: %s" % vehicle.is_armable
+print " System status: %s" % vehicle.system_status.state
+print " Mode: %s" % vehicle.mode.name    # settable
+
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print "Basic pre-arm checks"
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print " Waiting for vehicle to initialise..."
+        time.sleep(1)
+
+    print "Arming motors"
+    # Copter should arm in GUIDED mode
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print " Waiting for arming..."
+        time.sleep(1)
+
+    print "Taking off!"
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print " Altitude: ", vehicle.location.global_relative_frame.alt
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print "Reached target altitude"
+            break
+        time.sleep(1)
+
+arm_and_takeoff(1)
+
+
+# sleep so we can see the change in map
+time.sleep(15)
+
+
+#print "Returning to Launch"
+#vehicle.mode = VehicleMode("RTL")
+print("Landing...")
+vehicle.mode = VehicleMode("LAND")
+
+# Close vehicle object before exiting script
+vehicle.close()
+
+# Shut down
+print("Completed")
